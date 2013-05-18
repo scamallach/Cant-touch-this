39084e8e1a26acdf44802f206bb5186f72d67d93
Alex Killen <alexkillen@gmail.com>
2013-05-18 16:12:05 +0100

    Added assets: ratcage, wobble back.

--- CantTouchThis/CantTouchThis/Item.cs
+++ CantTouchThis/CantTouchThis/Item.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -7,9 +9,22 @@ namespace CantTouchThis
 {
     public class Item
     {
-        public Item()
+        Texture2D texture;
+        public Vector2 position { get; set; }
+
+        public Item(Texture2D texture, Vector2 position)
         {
-            
+            this.position = position;
+            this.texture = texture;
+        }
+
+        public void Update(GameTime gameTime)
+        {
+        }
+
+        public void Draw(SpriteBatch spriteBatch, GameTime gameTime)
+        {
+            spriteBatch.Draw(texture, position, Color.White);
         }
     }
 }
