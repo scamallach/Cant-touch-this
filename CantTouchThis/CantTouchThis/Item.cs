19b58db3e0e0e028d695e2dfbf8ed1d47299198e
Alex Killen <alexkillen@gmail.com>
2013-05-18 15:44:07 +0100

    Added wobble front asset. and empty Item class

---
+++ CantTouchThis/CantTouchThis/Item.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CantTouchThis
+{
+    public class Item
+    {
+        public Item()
+        {
+            
+        }
+    }
+}
