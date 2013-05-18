03fbadca6560c94d2abf588284e451c4aedcc89f
Clodagh ML <clodagh@scamallach.com>
2013-05-18 16:17:32 +0100

    Sprite sheets moved to player class

--- CantTouchThis/CantTouchThis/Player.cs
+++ CantTouchThis/CantTouchThis/Player.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -15,13 +16,33 @@ namespace CantTouchThis
         public int Width;
         public int Height;
 
+
         public List<Item> leftStack;
         public List<Item> rightStack;
+
+        public Texture2D CurrentWalk { get; protected set; }
+        protected Texture2D FrontWalk { get; set; }
+        protected Texture2D BackWalk { get; set; }
+        protected Texture2D FrontWobble { get; set; }
+        protected Texture2D BackWobble { get; set; }
+
+        //public PlayerL 
+
 
         public Player(int width, int height)
         {
             Width = width;
             Height = height;
+        }
+
+        public void LoadContent(Texture2D frontWalk, Texture2D backWalk, Texture2D frontWobble, Texture2D backWobble )
+        {
+            FrontWalk = frontWalk;
+            BackWalk = backWalk;
+            FrontWobble = frontWobble;
+            BackWobble = backWobble;
+
+            CurrentWalk = FrontWalk;
         }
 
         public void setPos(float x, float y) {
